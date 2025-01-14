ad6e2250661959b6a0caaef5beeb53ceaa3c1dff
Moritz-Mohr <moritz.mohr1@tha.de>
2025-01-14 10:08:24 +0100

    TempPushupBookingSystem

--- bookingsystem/views.py
+++ bookingsystem/views.py
@@ -7,6 +7,11 @@ from .forms import CustomUserCreationForm, CustomAuthenticationForm
 from .models import Booking, Room
 from django.db.models import Q
 from datetime import datetime, timedelta
+<<<<<<< Updated upstream
+=======
+from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
+>>>>>>> Stashed changes
 import json
 
 #LOGIN AND REGISTER
@@ -48,7 +53,19 @@ def bookingsystem(request):
     hours = range(8, 19)  # Hours from 8 AM to 6 PM
     minutes = range(0, 60, 5)  # Minutes from 00 to 55 in 5-minute intervals
 
+<<<<<<< Updated upstream
 
+=======
+    listofpossiblerooms = []
+
+    tempdate=None
+    tempstart_time=None
+    tempend_time=None
+    temproom_type=None
+    tempbuilding=None
+    tempgroup_size=None
+    temppurpose=None
+>>>>>>> Stashed changes
     
     # Handle form submission
     if request.method == 'POST':
@@ -71,6 +88,7 @@ def bookingsystem(request):
                 #Algorythem if the room is free in this Time
 
 
+<<<<<<< Updated upstream
                 listofrooms = None
 
             elif temproom_type == "sharedroom":
@@ -116,6 +134,112 @@ def bookingsystem(request):
 
 
 
+=======
+                bookingpossible = True
+
+                
+                
+                for room in possiblerooms:
+                    buchungen = Booking.objects.filter(roomid=room.room_id)
+                    
+                    bookingpossible = True
+
+                    for buchung in buchungen:
+                        
+                        if bookingpossible == True :
+
+                            if buchung.start_time < tempstart_time <= buchung.end_time :
+                                if buchung.start_time <= tempend_time < buchung.end_time:
+                                    bookingpossible = True
+                                else :
+                                    bookingpossible = False
+                            else:
+                                bookingpossible = False
+                                
+                        else :
+                            break
+
+                    
+                    if bookingpossible == True :
+                        listofpossiblerooms.append(room.room_id)
+
+
+            elif temproom_type == "sharedroom":
+
+                #Algorythem if in the Rooms seats are Emty
+
+                possiblerooms = Room.objects.filter(Q(building=tempbuilding) & Q(capacity__lte=tempgroup_size))
+
+
+                bookingpossible = True
+
+                
+                
+                for room in possiblerooms:
+                    buchungen = Booking.objects.filter(roomid=room.room_id)
+                    
+                    
+                    step = timedelta(minutes=5)
+
+                    # Iteration von starttime bis endtime
+                    current_time = tempstart_time
+                    while current_time <= tempend_time:
+
+                        peopleinroom = 0
+
+                        for buchung in buchungen:
+
+                            buchungssize = buchung.group_size 
+
+                            if buchung.start_time <= current_time < buchung.end_time :
+                                    peopleinroom = peopleinroom + buchungssize
+                                    
+                            
+                        if not peopleinroom <= room.size :
+                            bookingpossible = False
+                            break
+                          
+                        current_time += step
+                    
+
+
+
+   
+                    if bookingpossible == True :
+                        listofpossiblerooms.append(room.room_id)
+
+
+            return redirect('booking_suggestion')  # Redirect to a dashboard or success page
+        
+
+        except Exception as e:
+                    print(f"Booking error: {e}")
+                    return render(request, 'bookingsystem.html', {
+                        'error': 'Booking failed. Please try again.',
+                        'hours': hours,
+                        'minutes': minutes
+                    })       
+
+    request.session['tempdata'] = {
+            'date': tempdate,
+            'start_time': tempstart_time,
+            'end_time': tempend_time,
+            'room_type': temproom_type,
+            'building': tempbuilding,
+            'group_size': tempgroup_size,
+            'purpose': temppurpose,
+        }
+
+    request.session['listofpossiblerooms'] = listofpossiblerooms
+
+    return render(request, 'bookingsystem.html', {
+        'hours': hours,
+        'minutes': minutes
+    })           
+
+
+"""
+>>>>>>> Stashed changes
             # Create a new booking object with current user and form data
             booking = Booking(
                 user=request.user,
@@ -129,23 +253,84 @@ def bookingsystem(request):
             )
             booking.save()
             return redirect('dashboard')  # Redirect to a dashboard or success page
-        except Exception as e:
-            print(f"Booking error: {e}")
-            return render(request, 'bookingsystem.html', {
-                'error': 'Booking failed. Please try again.',
-                'hours': hours,
-                'minutes': minutes
-            })
+
+            
  # If it's a GET request, just render the booking form with the time selection options
     return render(request, 'bookingsystem.html', {
         'hours': hours,
         'minutes': minutes
     })
+"""
 
 #BOOKING suggestion
 @login_required
 def booking_suggestion(request):
+
+<<<<<<< Updated upstream
+#BOOKING suggestion
+@login_required
+def booking_suggestion(request):
     return render(request, 'booking_suggestion.html')
+=======
+
+    tempdata = request.session.get('tempdata', {})  # Standardwert ist {} falls keine Daten vorhanden sind
+    listofpossiblerooms = request.session.get('listofpossiblerooms', [])  # Abrufen der Buchungen, Standardwert ist eine leere Liste
+    
+    suggested_room_id = listofpossiblerooms.pop[0]
+
+
+    suggested_room= get_object_or_404(Room, id=suggested_room_id)
+
+    suggested_room_name = suggested_room.name
+
+    dropdown_rooms = listofpossiblerooms[1:]
+
+    tempdate = tempdata.get('date', '')  # Datum
+    tempstart_time = tempdata.get('start_time', '')  # Startzeit
+    tempend_time = tempdata.get('end_time', '')  # Endzeit
+    temproom_type = tempdata.get('room_type', '')  # Raumtyp
+    tempbuilding = tempdata.get('building', '')  # Gebäude
+    tempgroup_size = tempdata.get('group_size', 0)  # Gruppengröße (mit Defaultwert 0)
+    temppurpose = tempdata.get('purpose', '')  # Zweck
+
+    
+
+    options = dropdown_rooms
+
+    textforbutton = f"Der Raum {suggested_room_name} passt am Besten "
+    
+    if request.method == "POST2":
+        booking = Booking(
+                user=request.user,
+                date=tempdate,
+                start_time=tempstart_time,
+                end_time=tempend_time,
+                room_type=temproom_type,
+                building=tempbuilding,
+                group_size=tempgroup_size,
+                purpose=temppurpose
+            )
+        booking.save()
+        return redirect('my_bookings')
+
+    if request.method == 'POST':
+        # Das ausgewählte Item wird durch den POST-Request erhalten
+        selected_item = request.POST.get('selected_option', None)
+        
+        # Hier kannst du das ausgewählte Item weiterverarbeiten (z.B. in der Datenbank speichern oder eine Antwort anzeigen)
+        if selected_item:
+            # Zum Beispiel: Rückmeldung an den Benutzer
+            context = {'message': f"Du hast {selected_item} ausgewählt!"}
+            return render(request, 'booking_suggestion.html', context)
+
+
+    
+
+
+
+    # Wenn der POST-Request nicht vorliegt, wird das Dropdown angezeigt
+    return render(request, 'booking_suggestion.html', {'options': options})
+>>>>>>> Stashed changes
 
 #DASHBOARD
 
